bc511f420383c335b5a7b4ebddda6b2f82d8fda8
Marshall-Sun <910204019@qq.com>
2020-06-16 21:17:20 +0800

    del view

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,8 +5,6 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { AuthGuard } from './auth/auth.guard';
-import { RegisterViewComponent } from './register-view/register-view.component';
-
 
 const routes: Routes = [
   {
@@ -20,7 +18,6 @@ const routes: Routes = [
     data: { title: 'Users List' }
   },
   { path: 'register', component: RegisterComponent },
-  { path: 'register-view', component: RegisterViewComponent },
   { path: 'user', component: UserDetailComponent, canActivate: [AuthGuard], },
   { path: '**', component: PageNotFoundComponent },
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -24,7 +24,6 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { AppRoutingModule } from './app-routing.module';
-import { RegisterViewComponent } from './register-view/register-view.component';
 
 registerLocaleData(en);
 
@@ -35,7 +34,6 @@ registerLocaleData(en);
     UserDetailComponent,
     PageNotFoundComponent,
     LoginComponent,
-    RegisterViewComponent
   ],
   imports: [
     BrowserModule,
